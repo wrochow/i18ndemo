0b5ce4a70258440b9c0ef79b0634e7bd45e8b644
Waldo Rochow <waldo@rochow.info>
2023-10-26 15:07:07 -0400

    Spring Initializer

---
+++ src/test/java/ca/innolab/i18ndemo/I18nDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.innolab.i18ndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
